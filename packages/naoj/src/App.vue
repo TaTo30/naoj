e04cab4d9c752620f41b95bf78955567ad875131
TaTo30 <aldo.hernandez3012@gmail.com>
2026-08-27 14:10:13 -0600

    initial commit

---
+++ packages/naoj/src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { App } from "@naoj/components"
+</script>
+
+<template>
+  <div>
+    <aside>
+      <div>
+        database selector goes here
+      </div>
+      <div>
+        dynamic modules goes here
+      </div>
+      <div>
+        <App />
+        settings and other dynamic modules go here
+      </div>
+    </aside>
+    <main>
+      <div>
+        tabs go here
+      </div>
+      <div>
+        main content go here
+      </div>
+      <div>
+        status line goes here
+      </div>
+    </main>
+  </div>
+</template>
+
+<style scoped></style>
